2d4e9c754040520c928f63e632bc8310e09fd3ba
chenzixin <17826806525@163.com>
2020-09-25 17:21:44 +0800

    9.25

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * State Hook
+ */
+// import React, { useState } from 'react';
 
-function App() {
+// function App() {
+//   // useState()
+//   // 1. 方法里面唯一的参数就是初始state
+//   // 2. 返回值为 当前state 和 更新 state 的函数
+//   const [count, setCount] = useState(0)
+//   return (
+//     <div>
+//       <p>you clicked {count} times</p>
+//       <button onClick={() => {
+//         // 3. 更新 state 的函数接受一个新值来更新 state
+//         setCount(count + 1)
+//       }}>click me</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+/**
+ * Effect Hook
+ */
+import React, {useState, useEffect} from 'react'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+  
+  // 类似 componentDidMount 和 componentDidUpdate
+  useEffect(() => {
+    document.title = `you click ${count} times`
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>you clicked {count} times</p>
+      <button onClick={() => {
+        setCount(count + 1)
+      }}>click me</button>
     </div>
-  );
+  )
 }
 
-export default App;
